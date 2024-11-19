807792d4f7e77d9d2aad6b1d0eb60a48000532cc
lgr-2024 <lgr@devtools.kr>
2024-11-19 19:28:26 +0900

    🎨[REFACTORING] more edit 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
-  <div>Hello World 3123123123123123asdasdasdasda</div>
+  <div>Hello World 3123123123123123asdasdasdasdaqedqdqd</div>
 </template>
 
 <style scoped>
