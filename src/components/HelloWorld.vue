3e86e9b5c4649bb8fa1d60416011f1864f0536b6
lgr-2024 <lgr@devtools.kr>
2024-11-21 17:42:17 +0900

    젠킨스 테스트 6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
-  <div>Jenkins Test 5</div>
+  <div>Jenkins Test 6</div>
 </template>
 
 <style scoped>
