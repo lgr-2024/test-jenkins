8dc958f5ee0a1935d920cb3d0b2f33d0785dc9ba
lgr-2024 <lgr@devtools.kr>
2024-11-21 17:50:32 +0900

    Jenkins Test 8

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
-  <div>Jenkins Test 7</div>
+  <div>Jenkins Test 8</div>
 </template>
 
 <style scoped>
