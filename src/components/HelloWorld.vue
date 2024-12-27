ddda2aa1222b84ae3bfccf44209bc5b3ede0b5f4
lgr-2024 <lgr@devtools.kr>
2024-12-27 15:48:28 +0900

    test 90

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ defineProps({
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
   </div>
-  <div>Jenkins Test 89</div>
+  <div>Jenkins Test 90</div>
 </template>
 
 <style scoped>
